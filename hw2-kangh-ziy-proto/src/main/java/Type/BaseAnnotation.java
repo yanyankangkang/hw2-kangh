d20daf0da1fa3a656f2c283f95c28b0bf87ffaf1
yanyankangkang <kanghuangnj@gmail.com>
2014-10-09 19:30:50 -0400

    m

---
+++ hw2-kangh-ziy-proto/src/main/java/Type/BaseAnnotation.java
@@ -0,0 +1,123 @@
+
+
+/* First created by JCasGen Thu Oct 09 16:48:44 EDT 2014 */
+package Type;
+
+import org.apache.uima.jcas.JCas; 
+import org.apache.uima.jcas.JCasRegistry;
+import org.apache.uima.jcas.cas.TOP_Type;
+
+import edu.cmu.deiis.types.Annotation;
+
+
+/** 
+ * Updated by JCasGen Thu Oct 09 18:10:12 EDT 2014
+ * XML source: /home/mac/workspace/11791/hw2-kangh/hw2-kangh-ziy-proto/src/main/resources/descriptors/deiis_types.xml
+ * @generated */
+public class BaseAnnotation extends Annotation {
+  /** @generated
+   * @ordered 
+   */
+  @SuppressWarnings ("hiding")
+  public final static int typeIndexID = JCasRegistry.register(BaseAnnotation.class);
+  /** @generated
+   * @ordered 
+   */
+  @SuppressWarnings ("hiding")
+  public final static int type = typeIndexID;
+  /** @generated
+   * @return index of the type  
+   */
+  @Override
+  public              int getTypeIndexID() {return typeIndexID;}
+ 
+  /** Never called.  Disable default constructor
+   * @generated */
+  protected BaseAnnotation() {/* intentionally empty block */}
+    
+  /** Internal - constructor used by generator 
+   * @generated
+   * @param addr low level Feature Structure reference
+   * @param type the type of this Feature Structure 
+   */
+  public BaseAnnotation(int addr, TOP_Type type) {
+    super(addr, type);
+    readObject();
+  }
+  
+  /** @generated
+   * @param jcas JCas to which this Feature Structure belongs 
+   */
+  public BaseAnnotation(JCas jcas) {
+    super(jcas);
+    readObject();   
+  } 
+
+  /** @generated
+   * @param jcas JCas to which this Feature Structure belongs
+   * @param begin offset to the begin spot in the SofA
+   * @param end offset to the end spot in the SofA 
+  */  
+  public BaseAnnotation(JCas jcas, int begin, int end) {
+    super(jcas);
+    setBegin(begin);
+    setEnd(end);
+    readObject();
+  }   
+
+  /** 
+   * <!-- begin-user-doc -->
+   * Write your own initialization here
+   * <!-- end-user-doc -->
+   *
+   * @generated modifiable 
+   */
+  private void readObject() {/*default - does nothing empty block */}
+     
+ 
+    
+  //*--------------*
+  //* Feature: ID
+
+  /** getter for ID - gets ID
+   * @generated
+   * @return value of the feature 
+   */
+  public String getID() {
+    if (BaseAnnotation_Type.featOkTst && ((BaseAnnotation_Type)jcasType).casFeat_ID == null)
+      jcasType.jcas.throwFeatMissing("ID", "Type.BaseAnnotation");
+    return jcasType.ll_cas.ll_getStringValue(addr, ((BaseAnnotation_Type)jcasType).casFeatCode_ID);}
+    
+  /** setter for ID - sets ID 
+   * @generated
+   * @param v value to set into the feature 
+   */
+  public void setID(String v) {
+    if (BaseAnnotation_Type.featOkTst && ((BaseAnnotation_Type)jcasType).casFeat_ID == null)
+      jcasType.jcas.throwFeatMissing("ID", "Type.BaseAnnotation");
+    jcasType.ll_cas.ll_setStringValue(addr, ((BaseAnnotation_Type)jcasType).casFeatCode_ID, v);}    
+   
+    
+  //*--------------*
+  //* Feature: Words
+
+  /** getter for Words - gets a line words
+   * @generated
+   * @return value of the feature 
+   */
+  public String getWords() {
+    if (BaseAnnotation_Type.featOkTst && ((BaseAnnotation_Type)jcasType).casFeat_Words == null)
+      jcasType.jcas.throwFeatMissing("Words", "Type.BaseAnnotation");
+    return jcasType.ll_cas.ll_getStringValue(addr, ((BaseAnnotation_Type)jcasType).casFeatCode_Words);}
+    
+  /** setter for Words - sets a line words 
+   * @generated
+   * @param v value to set into the feature 
+   */
+  public void setWords(String v) {
+    if (BaseAnnotation_Type.featOkTst && ((BaseAnnotation_Type)jcasType).casFeat_Words == null)
+      jcasType.jcas.throwFeatMissing("Words", "Type.BaseAnnotation");
+    jcasType.ll_cas.ll_setStringValue(addr, ((BaseAnnotation_Type)jcasType).casFeatCode_Words, v);}    
+  }
+
+    
---
+++ hw2-kangh-ziy-proto/src/main/java/Type/BaseAnnotation_Type.java
@@ -0,0 +1,121 @@
+
+/* First created by JCasGen Thu Oct 09 16:48:44 EDT 2014 */
+package Type;
+
+import org.apache.uima.jcas.JCas;
+import org.apache.uima.jcas.JCasRegistry;
+import org.apache.uima.cas.impl.CASImpl;
+import org.apache.uima.cas.impl.FSGenerator;
+import org.apache.uima.cas.FeatureStructure;
+import org.apache.uima.cas.impl.TypeImpl;
+import org.apache.uima.cas.Type;
+import org.apache.uima.cas.impl.FeatureImpl;
+import org.apache.uima.cas.Feature;
+import edu.cmu.deiis.types.Annotation_Type;
+
+/** 
+ * Updated by JCasGen Thu Oct 09 18:10:12 EDT 2014
+ *  */
+public class BaseAnnotation_Type extends Annotation_Type {
+  /**  
+   * @return the generator for this type
+   */
+  @Override
+  protected FSGenerator getFSGenerator() {return fsGenerator;}
+  /**  */
+  private final FSGenerator fsGenerator = 
+    new FSGenerator() {
+      public FeatureStructure createFS(int addr, CASImpl cas) {
+  			 if (BaseAnnotation_Type.this.useExistingInstance) {
+  			   // Return eq fs instance if already created
+  		     FeatureStructure fs = BaseAnnotation_Type.this.jcas.getJfsFromCaddr(addr);
+  		     if (null == fs) {
+  		       fs = new BaseAnnotation(addr, BaseAnnotation_Type.this);
+  			   BaseAnnotation_Type.this.jcas.putJfsFromCaddr(addr, fs);
+  			   return fs;
+  		     }
+  		     return fs;
+        } else return new BaseAnnotation(addr, BaseAnnotation_Type.this);
+  	  }
+    };
+  /**  */
+  @SuppressWarnings ("hiding")
+  public final static int typeIndexID = BaseAnnotation.typeIndexID;
+  /**  
+      */
+  @SuppressWarnings ("hiding")
+  public final static boolean featOkTst = JCasRegistry.getFeatOkTst("Type.BaseAnnotation");
+ 
+  /**  */
+  final Feature casFeat_ID;
+  /**  */
+  final int     casFeatCode_ID;
+  /** 
+   * @param addr low level Feature Structure reference
+   * @return the feature value 
+   */ 
+  public String getID(int addr) {
+        if (featOkTst && casFeat_ID == null)
+      jcas.throwFeatMissing("ID", "Type.BaseAnnotation");
+    return ll_cas.ll_getStringValue(addr, casFeatCode_ID);
+  }
+  /** 
+   * @param addr low level Feature Structure reference
+   * @param v value to set 
+   */    
+  public void setID(int addr, String v) {
+        if (featOkTst && casFeat_ID == null)
+      jcas.throwFeatMissing("ID", "Type.BaseAnnotation");
+    ll_cas.ll_setStringValue(addr, casFeatCode_ID, v);}
+    
+  
+ 
+  /**  */
+  final Feature casFeat_Words;
+  /**  */
+  final int     casFeatCode_Words;
+  /** 
+   * @param addr low level Feature Structure reference
+   * @return the feature value 
+   */ 
+  public String getWords(int addr) {
+        if (featOkTst && casFeat_Words == null)
+      jcas.throwFeatMissing("Words", "Type.BaseAnnotation");
+    return ll_cas.ll_getStringValue(addr, casFeatCode_Words);
+  }
+  /** 
+   * @param addr low level Feature Structure reference
+   * @param v value to set 
+   */    
+  public void setWords(int addr, String v) {
+        if (featOkTst && casFeat_Words == null)
+      jcas.throwFeatMissing("Words", "Type.BaseAnnotation");
+    ll_cas.ll_setStringValue(addr, casFeatCode_Words, v);}
+    
+  
+
+
+
+  /** initialize variables to correspond with Cas Type and Features
+	 * 
+	 * @param jcas JCas
+	 * @param casType Type 
+	 */
+  public BaseAnnotation_Type(JCas jcas, Type casType) {
+    super(jcas, casType);
+    casImpl.getFSClassRegistry().addGeneratorForType((TypeImpl)this.casType, getFSGenerator());
+
+ 
+    casFeat_ID = jcas.getRequiredFeatureDE(casType, "ID", "uima.cas.String", featOkTst);
+    casFeatCode_ID  = (null == casFeat_ID) ? JCas.INVALID_FEATURE_CODE : ((FeatureImpl)casFeat_ID).getCode();
+
+ 
+    casFeat_Words = jcas.getRequiredFeatureDE(casType, "Words", "uima.cas.String", featOkTst);
+    casFeatCode_Words  = (null == casFeat_Words) ? JCas.INVALID_FEATURE_CODE : ((FeatureImpl)casFeat_Words).getCode();
+
+  }
+}
+
+
+
+    
